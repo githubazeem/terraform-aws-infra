2c20e36a9c94e2cc472f8f21067f28a42e16603e
githubazeem <azeemsiddiqui111@gmail.com>
2026-07-12 01:28:23 +0530

    nothing

--- terraform/aws/elb/dev/main.tf
+++ terraform/aws/elb/dev/main.tf
@@ -9,7 +9,7 @@ module "aws_subnet" {
   depends_on = [module.aws_vpc]
   source = "../../modules/subnet"
    
-  vpc_id   = module.vpc.vpc_id
+  vpc_id   = module.vpc_vpc.vpc_id
   subnetchild = var.subnetroot
 }
 
